f0842c7645723a9885d1c3df8c536ac4c81f5458
rtaylor02 <taylor.rod.j@gmail.com>
2022-07-14 09:37:18 +0100

    Everything committed

---
+++ Java/GitDemo/src/mypackage/Main.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class Main {
+    public static void main(String...args) {
+        System.out.println("This is Git Demo project");
+        System.out.println("Another line");
+    }
+}
