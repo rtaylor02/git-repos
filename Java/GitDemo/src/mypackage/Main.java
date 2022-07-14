0861cef332b431249b188286dc846c07301ca44e
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2022-07-14 09:46:01 +0100

    Make "another line" to 2

--- Java/GitDemo/src/mypackage/Main.java
+++ Java/GitDemo/src/mypackage/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String...args) {
         System.out.println("This is Git Demo project");
         System.out.println("Another line");
+        System.out.println("Another line");
     }
 }
