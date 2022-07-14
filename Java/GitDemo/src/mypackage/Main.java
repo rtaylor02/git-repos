a1e4df4e9dd64f5ea1eb7ad0080083c9dc9f9e0e
rtaylor02 <taylor.rod.j@gmail.com>
2022-07-14 09:45:23 +0100

    Delete 4 "another line"

--- Java/GitDemo/src/mypackage/Main.java
+++ Java/GitDemo/src/mypackage/Main.java
@@ -4,9 +4,5 @@ public class Main {
     public static void main(String...args) {
         System.out.println("This is Git Demo project");
         System.out.println("Another line");
-        System.out.println("Another line");
-        System.out.println("Another line");
-        System.out.println("Another line");
-        System.out.println("Another line");
     }
 }
