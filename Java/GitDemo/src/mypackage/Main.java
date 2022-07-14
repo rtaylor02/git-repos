584624660ada01b0c776a622c7d2752895dc3e26
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2022-07-14 09:42:58 +0100

    Make 5 lines of "another line"

--- Java/GitDemo/src/mypackage/Main.java
+++ Java/GitDemo/src/mypackage/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Another line");
         System.out.println("Another line");
         System.out.println("Another line");
+        System.out.println("Another line");
+        System.out.println("Another line");
     }
 }
