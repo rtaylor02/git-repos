589a074406c943c74b7e7d23f4672a6edcf3f2a3
rtaylor02 <taylor.rod.j@gmail.com>
2022-09-06 13:42:48 +0100

    Routine commit

--- REST/restful-web-services/src/main/java/com/rtaylor02/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ REST/restful-web-services/src/main/java/com/rtaylor02/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,11 @@ package com.rtaylor02.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,5 +14,23 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+//		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//		localeResolver.setDefaultLocale(Locale.US);
+//		return localeResolver;
+
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+//	@Bean
+//	public ResourceBundleMessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasename("messages");
+//		return messageSource;
+//	}
 
 }
